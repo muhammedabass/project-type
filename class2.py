53ab8f0ebf58dac4312e68c2bd4a5e27f46e3d05
muhammedabass <148213100+muhammedabass@users.noreply.github.com>
2023-11-20 10:06:14 +0100

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,55 @@
+x=34
+print(x)
+print(type(x))
+
+x="hello world"
+print(x)
+print(len(x))
+print(x[-1])
+print(x[2:7])
+print(x[:8])
+print(x[2:])
+print(x[-3])
+print(x[-9:-3])
+print(x[-11:-6])
+
+print(x.lower())
+print(x.upper())
+print(x.capitalize())
+print(x.count("o"))
+x ="   hello   "
+print(x)
+print(x.lstrip())
+print(x.rstrip())
+#name= " parach"
+#print(x + name)
+#print(x.strip() + name)
+#name= input("myname: ")
+#greet = " hello "
+#print(greet)
+#print(name)
+
+txt="i like banana"
+x = txt.replace("banana", "mango")
+print(x)
+
+
+num2 = 13
+
+check = 3**3
+print(check)
+
+check = 3 % 3
+print(check)
+check = 5 % 3
+print(check)
+check = 3 % 5
+print(check)
+check = 8 // 3
+print(check)
+check = 3 // 4
+print(check)
+check =5 // 4
+print(check)
+x = 3*3
+print(x)
